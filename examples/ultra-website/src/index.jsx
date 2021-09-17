96071c3012d81060a333b1e7ae40e7a51eea3ebe
mashaal <omar@teacups.io>
2021-09-18 09:46:27 +1000

    wip

--- examples/ultra-website/src/index.jsx
+++ examples/ultra-website/src/index.jsx
@@ -6,38 +6,7 @@ const fetcher = (url) => fetch(url).then((r) => r.json());
 const link =
   "https://github.com/exhibitionist-digital/ultra/tree/master/examples";
 
-const Examples = () => {
-  const { data } = useSWR(`ULTRA_URL/data.json`, fetcher);
-  const { examples } = data;
-  return (
-    <>
-      <h3>Check out these examples</h3>
-      <section>
-        {examples.map((
-          ex,
-        ) => (
-          <a target="_blank" href={ex.url} className="ex" rel="noopener">
-            <div className="emoji">{ex.emoji}</div>
-            <strong>{ex.title}</strong>
-            <p>{ex.description}</p>
-          </a>
-        ))}
-      </section>
-      <p>
-        Source code for these can be found on&nbsp;
-        <a href={link} target="_blank" rel="noopener">GitHub</a>
-      </p>
-      <h4>Go ahead, run Lighthouse, we dare you</h4>
-      <img
-        className="lighthouse"
-        width="100%"
-        height="auto"
-        alt="perfect lighthouse score"
-        src="/lighthouse.png"
-      />
-    </>
-  );
-};
+
 
 const Index = () => {
   return (
@@ -58,9 +27,7 @@ const Index = () => {
       >
         View on GitHub
       </a>
-      <Suspense fallback={null}>
-        <Examples />
-      </Suspense>
+      
     </main>
   );
 };
