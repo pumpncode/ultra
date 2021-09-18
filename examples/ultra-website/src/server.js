f64ca83e5a6c1f87e8848d860efba7b02364c4a2
mashaal <omar@teacups.io>
2021-09-18 21:28:46 +1000

    Update server.js

--- examples/ultra-website/src/server.js
+++ examples/ultra-website/src/server.js
@@ -55,8 +55,8 @@ async function handleRequest(event) {
 
   // static files in pages
   if (buildFiles.indexOf(url.pathname) >= 0) {
-    console.log('cwd', Deno.cwd());
-    const file = await Deno.readFile(join(Deno.cwd(), url.pathname));
+    console.log('cwd', Deno.cwd(), url.pathname);
+    const file = await Deno.readTextFile(join(Deno.cwd(), url.pathname));
     return new Response(file, {
       headers: {
         "content-type": lookup(url.pathname),
